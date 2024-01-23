28479de34332073e342fa11f0b1a081157cc7290
Lisin_sw23@top-academy.ru <Lisin_sw23@top-academy.ru>
2024-01-23 10:54:33 +1000

    ChangeCommentsTest

--- Geometry2/main.cpp
+++ Geometry2/main.cpp
@@ -2,6 +2,10 @@
 #include<conio.h>
 #include<cstdlib>
 using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
+
 //#define SQUARE
 //#define TRIANGLE1
 //#define TRIANGLE2
@@ -11,11 +15,13 @@ using namespace std;
 //#define PLUSMINUSSQUARE
 //#define CHESS
 //#define CHESS_EXTAND 
-#define TRIANGLE_PASCAL
+//#define CHESS_EXTAND_TWO_FOR
+//#define TRIANGLE_PASCAL
 //#define TRIANGLE_PASCAL_2
+//#define TRIANGLE_PASCAL_3_BEST
 
-#define INPUT cout << "Введите размер фигуры: "; cin >> n;
-#define INPUT_DESK cout << "Введите размер доски: "; cin >> n;
+#define INPUT cout << "Enter figure size: "; cin >> n;
+#define INPUT_DESK cout << "Enter board size: "; cin >> n;
 
 long long factorial(int n) {
 	if (n == 0 || n == 1) return 1;
@@ -33,55 +39,55 @@ void main() {
 
 #ifdef SQUARE
 	INPUT
-	for (int i = 0; i < n; i++) {
-		for (int i = 0; i < n; i++) cout << "* ";
-		cout << endl;
-	}
+		for (int i = 0; i < n; i++) {
+			for (int i = 0; i < n; i++) cout << "* ";
+			cout << endl;
+		}
 
 #endif // SQUARE
 
 #ifdef TRIANGLE1
 	INPUT
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j <= i; j++) cout << "* ";
-		cout << endl;
-	}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <= i; j++) cout << "* ";
+			cout << endl;
+		}
 #endif // TRIANGLE1
 
 #ifdef TRIANGLE2
 	INPUT
-	for (int i = 0; i < n; i++) {
-		for (int j = i; j < n; j++) cout << "* ";
-		cout << endl;
-	}
+		for (int i = 0; i < n; i++) {
+			for (int j = i; j < n; j++) cout << "* ";
+			cout << endl;
+		}
 
 #endif // TRIANGLE2
 
 #ifdef TRIANGLE3
 	INPUT
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < i; j++)cout << "  ";
-		for (int j = i; j < n; j++)cout << "* ";
-		cout << endl;
-	}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < i; j++)cout << "  ";
+			for (int j = i; j < n; j++)cout << "* ";
+			cout << endl;
+		}
 #endif // TRIANGLE3
 
 #ifdef TRIANGLE4
 	INPUT
-	for (int i = 0; i < n; i++) {
-		for (int j = i; j < n; j++)cout << "  ";
-		for (int j = 0; j <= i; j++)cout << "* ";
-		cout << endl;
-	}
+		for (int i = 0; i < n; i++) {
+			for (int j = i; j < n; j++)cout << "  ";
+			for (int j = 0; j <= i; j++)cout << "* ";
+			cout << endl;
+		}
 #endif // TRIANGLE4
 
 #ifdef RHOMBUS
 	INPUT
-	for (int i = 0; i < n; i++) {
-		for (int j = i; j <= n; j++) cout << (j == n ? "/" : " ");
-		for (int j = 0; j <= 2 * i; j++) cout << (j == 2 * i ? "\\" : " ");
-		cout << endl;
-	}
+		for (int i = 0; i < n; i++) {
+			for (int j = i; j <= n; j++) cout << (j == n ? "/" : " ");
+			for (int j = 0; j <= 2 * i; j++) cout << (j == 2 * i ? "\\" : " ");
+			cout << endl;
+		}
 	for (int i = 1; i <= n; i++) {
 		for (int j = 0; j <= i; j++) cout << (j == i ? "\\" : " ");
 		for (int j = 2 * i; j <= 2 * n; j++)cout << (j == 2 * n ? "/" : " ");
@@ -91,20 +97,20 @@ void main() {
 
 #ifdef PLUSMINUSSQUARE
 	INPUT
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++)
-		{
-			cout << (i % 2 == j % 2 ? "+ " : "- ");
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++)
+			{
+				cout << (i % 2 == j % 2 ? "+ " : "- ");
+			}
+			cout << endl;
 		}
-		cout << endl;
-	}
 #endif // PLUSMINUSSQUARE
 
 #ifdef CHESS
 
 	INPUT_DESK
-	n += 1;
-	setlocale(LC_ALL, "C"); // кодировка по умолчанию defolt
+		n += 1;
+	setlocale(LC_ALL, "C"); // default encoding
 	for (int i = 0; i <= n; i++)
 	{
 		for (int j = 0; j <= n; j++) {
@@ -118,7 +124,7 @@ void main() {
 		cout << endl;
 	}
 	setlocale(LC_ALL, "");
-	cout << endl << "Введите любую клавишу для продолжения....";
+	cout << endl << "To continue enter any key....";
 
 	char key = _getch();
 
@@ -126,7 +132,7 @@ void main() {
 
 #ifdef CHESS_EXTAND
 	INPUT_DESK
-	cout << endl;
+		cout << endl;
 
 	for (int k = 0; k < n; k++)
 	{
@@ -134,16 +140,57 @@ void main() {
 		{
 			for (int q = 0; q < n; q++)
 			{
-				for (int j = 0; j < n; j++) cout << (q % 2 == k % 2 ? "* " : "  ");
+				for (int j = 0; j < n; j++)
+					cout << (q % 2 == k % 2 ? "* " : "  ");
 			}
 			cout << endl;
 
 		}
 	}
-	cout << endl << "Для продолжения введите любую клавишу....";
+	cout << endl << "To continue enter any key....";
 	char key = _getch();
 
 #endif // CHESS_EXTAND
+
+#ifdef CHESS_EXTAND_TWO_FOR
+	INPUT_DESK
+		cout << endl;
+	int r = 1;
+
+	for (int i = 1; i <= n * n; i++) // go to the down
+	{
+		for (int j = 1; j <= n * n; j++) // go to the right
+		{
+			cout << (r % 2 == j % 2 ? "* " : "  ");
+			if (!(j % n)) r++; // if the square ends on the right
+			r++;
+		}
+		if (!(i % n)) r++; // if the square ends on the down
+		cout << endl;
+	}
+#endif // CHESS_EXTAND_TWO_FOR
+
+	INPUT_DESK
+		cout << endl;
+	/*
+	for (int i = 0; i < n * n; i++)
+	{
+		for (int j = 0; j < n * n; j++)
+		{
+			cout << (((i/ n) % 2 == (j / n) % 2) ? "* " : "  ");
+		}
+		cout << endl;
+	}
+	*/
+
+	for (int i = 0; i < n * n; i++) {
+		for (int j = 0; j < n * n; j++)
+
+			cout << (((i/n +j/n) % 2) ? "  " : "* ");
+
+		cout << endl;
+	}
+
 
 #ifdef TRIANGLE_PASCAL
 	INPUT
@@ -183,7 +230,7 @@ void main() {
 			C= Cm / (Ck * Ck_m);
 			while (C != 0) {
 				C /= 10;
-				Cwide++;
+				Cwide++; // 
 			}
 			std::cout << Cm / (Ck * Ck_m);
 			for (int s = 0; s <= ((wide - Cwide)); s++)std::cout << " ";
@@ -206,7 +253,7 @@ void main() {
 		f2 = f0 + f1;
 	}
 
-	// Оптимизация: вычисляем wide заранее, используя log10
+	// optimizaation: calculate the width in advance, use log10
 	while (f1 != 0) {
 		f1 /= 10;
 		wide++;
@@ -220,12 +267,12 @@ void main() {
 			long long C = factorial(i) / (factorial(j) * factorial(i - j));
 			cout << C;
 			int Cwide = 0;
-			// Вычисляем количество цифр в C
+			// calculate the number of digits in C
 			while (C != 0) {
 				C /= 10;
 				Cwide++;
 			}
-			// Выводим пробелы после C
+			// print spaces after C
 			for (int s = 0; s <= (wide - Cwide); s++) {
 				cout << " ";
 			}
@@ -233,5 +280,21 @@ void main() {
 		cout << endl;
 	}
 #endif // TRIANGLE_PASCAL_2
+
+
+#ifdef TRIANLE_PASCAL_3_BEST
+	INPUT
+
+		// Print Pascal's Triangle
+		for (int i = 0; i < n; i++) {
+			int coef = 1;
+			for (int j = 0; j <= i; j++) {
+				std::cout << coef << " ";
+				coef = coef * (i - j) / (j + 1);
+			}
+			std::cout << std::endl;
+		}
+#endif // TRIANLE_PASCAL_3_BEST
+
 
 }
